e71eedd0c9b414788c8497c52b6c44f6c3cf3d08
Kratos <nrupsis@gmail.com>
2018-03-12 18:30:54 -0400

    added db driver

--- server/server.py
+++ server/server.py
@@ -3,5 +3,9 @@ import json
 from datetime import date, timedelta
 from flask import Flask
 from flask_cors import CORS
+import psycopg2
+
+# Database connection
+conn = psycopg2.connect("dbname=test user=postgres")
 
 print("hello world")
