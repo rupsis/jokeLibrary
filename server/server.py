6fa70038bde30aac72cd53ee0d35c60fd7c220c8
Kratos <nrupsis@gmail.com>
2018-03-14 02:21:52 -0400

    inital route methods

--- server/server.py
+++ server/server.py
@@ -15,4 +15,31 @@ cur = db.cursor()
 app = Flask(__name__)
 CORS(app)
 
+# Get All
+@app.route("/jokes", methods=['GET'])
+def get_jokes():
+    jokes = "test"
+    return json.dumps(jokes)
+
+# Get by id
+@app.route("/jokes/<id>")
+def get_jokes(id):
+    pass
+
+# Create
+@app.route("/jokes", methods=['POST'])
+def create_joke():
+    pass
+
+# Update
+@app.route("/jokes/<id>")
+def update_joke(id):
+    pass
+
+# Delete
+@app.route("/jokes/<id>")
+def delete_joke(id):
+    pass
+
+
 print("server running")
